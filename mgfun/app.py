9b0b5f702fc6e0e9d59aeb62520d72e47ba10cea
mohamadrezagharaee27-oss <mohamadrezagharaee27@gmail.com>
2026-06-25 14:49:05 +0330

    Add files via upload

--- mgfun/app.py
+++ mgfun/app.py
@@ -349,7 +349,9 @@ def create_app(env=None):
 
 app = create_app()
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+    
+    app.run(debug=True, host='0.0.0.0', port="port")
